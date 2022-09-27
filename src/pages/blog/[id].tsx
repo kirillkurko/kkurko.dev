16791452cd6f7d307754e870f10c4d5236483122
Kirill Kurko <kirillkurko2609@gmail.com>
2022-09-27 19:52:49 +0300

    Some code structure changes

---
+++ src/pages/blog/[id].tsx
@@ -0,0 +1,80 @@
+// @ts-nocheck
+import { Fragment } from 'react';
+import Head from 'next/head';
+import { getDatabase, getPage, getBlocks } from '../../util/notion';
+import Link from 'next/link';
+import { databaseId } from './index';
+import styles from './post.module.css';
+import Text from '../../components/Text';
+import { renderBlock } from '../../util/notion/render';
+
+export default function Post({ page, blocks }) {
+  console.log(page, blocks);
+
+  if (!page || !blocks) {
+    return <div />;
+  }
+  return (
+    <div>
+      <Head>
+        <title>{page.properties.Name.title[0].plain_text}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <article className={styles.container}>
+        <h1 className={styles.name}>
+          <Text text={page.properties.Name.title} />
+        </h1>
+        <section>
+          {blocks.map((block) => (
+            <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+          ))}
+          <Link href='/blog'>
+            <a className={styles.back}>← Go home</a>
+          </Link>
+        </section>
+      </article>
+    </div>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const database = await getDatabase(databaseId);
+  return {
+    paths: database.map((page) => ({ params: { id: page.id } })),
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const { id } = context.params;
+  const page = await getPage(id);
+  const blocks = await getBlocks(id);
+
+  const childBlocks = await Promise.all(
+    blocks
+      .filter((block) => block.has_children)
+      .map(async (block) => {
+        return {
+          id: block.id,
+          children: await getBlocks(block.id),
+        };
+      }),
+  );
+  const blocksWithChildren = blocks.map((block) => {
+    if (block.has_children && !block[block.type].children) {
+      block[block.type].children = childBlocks.find(
+        (x) => x.id === block.id,
+      )?.children;
+    }
+    return block;
+  });
+
+  return {
+    props: {
+      page,
+      blocks: blocksWithChildren,
+    },
+    revalidate: 1,
+  };
+};
--- src/util/notion/render.tsx
+++ src/util/notion/render.tsx
@@ -1,10 +1,9 @@
+// @ts-nocheck
+/* eslint-disable */
+import Text from '@components/Text';
 import { Fragment } from 'react';
-import Head from 'next/head';
-import { getDatabase, getPage, getBlocks } from '../../util/notion';
 import Link from 'next/link';
-import { databaseId } from './index';
-import styles from './post.module.css';
-import Text from '../../components/Text';
+import styles from '../../pages/blog/post.module.css';
 
 const renderNestedList = (block) => {
   const { type } = block;
@@ -19,7 +18,7 @@ const renderNestedList = (block) => {
   return <ul>{value.children.map((block) => renderBlock(block))}</ul>;
 };
 
-const renderBlock = (block) => {
+export const renderBlock = (block) => {
   const { type, id } = block;
   const value = block[type];
 
@@ -131,75 +130,4 @@ const renderBlock = (block) => {
         type === 'unsupported' ? 'unsupported by Notion API' : type
       })`;
   }
-};
-
-export default function Post({ page, blocks }) {
-  console.log(page, blocks)
-
-  if (!page || !blocks) {
-    return <div />;
-  }
-  return (
-    <div>
-      <Head>
-        <title>{page.properties.Name.title[0].plain_text}</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <article className={styles.container}>
-        <h1 className={styles.name}>
-          <Text text={page.properties.Name.title} />
-        </h1>
-        <section>
-          {blocks.map((block) => (
-            <Fragment key={block.id}>{renderBlock(block)}</Fragment>
-          ))}
-          <Link href='/blog'>
-            <a className={styles.back}>← Go home</a>
-          </Link>
-        </section>
-      </article>
-    </div>
-  );
-}
-
-export const getStaticPaths = async () => {
-  const database = await getDatabase(databaseId);
-  return {
-    paths: database.map((page) => ({ params: { id: page.id } })),
-    fallback: true,
-  };
-};
-
-export const getStaticProps = async (context) => {
-  const { id } = context.params;
-  const page = await getPage(id);
-  const blocks = await getBlocks(id);
-
-  const childBlocks = await Promise.all(
-    blocks
-      .filter((block) => block.has_children)
-      .map(async (block) => {
-        return {
-          id: block.id,
-          children: await getBlocks(block.id),
-        };
-      }),
-  );
-  const blocksWithChildren = blocks.map((block) => {
-    if (block.has_children && !block[block.type].children) {
-      block[block.type].children = childBlocks.find(
-        (x) => x.id === block.id,
-      )?.children;
-    }
-    return block;
-  });
-
-  return {
-    props: {
-      page,
-      blocks: blocksWithChildren,
-    },
-    revalidate: 1,
-  };
 };

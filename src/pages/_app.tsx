f19c177f4fc369c5b3e446a9e11592a35f29a2cc
Kirill Kurko <kirillkurko2609@gmail.com>
2022-10-01 21:55:01 +0300

    Google Analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,9 @@ import GlobalStyle from '@theme/globalStyle';
 import { themes } from '@theme/theme';
 import Head from 'next/head';
 import Container from '@components/Container';
+import Script from 'next/script';
+
+const googleAnalyticsId = process.env.NOTION_DATABASE_ID;
 
 enum Mode {
   Dark = 'DARK',
@@ -21,6 +24,19 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <meta name='description' content='My personal website' />
         <link rel='shortcut icon' href='/favicon.ico' />
       </Head>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+        strategy='lazyOnload'
+      />
+      <Script id='google-analytics' strategy='lazyOnload'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${googleAnalyticsId});
+        `}
+      </Script>
       <GlobalStyle />
       <ThemeProvider theme={themes[MODE]}>
         <Container>

22699f8ad679888dcc6f09b4836e045d2253a8d2
Kirill Kurko <kirillkurko2609@gmail.com>
2023-02-12 17:59:50 +0300

    ArticlePreview structure updated

--- src/components/ArticlePreview.tsx
+++ src/components/ArticlePreview.tsx
@@ -18,14 +18,16 @@ const ArticlePreview = ({ blog }: Props) => {
   const link = `/blog/${blog.slug}`;
 
   return (
-    <li className='mb-4 border border-neutral-700 rounded-md py-4 px-8'>
+    <li className='mb-4 border border-neutral-700 rounded-md'>
       <NextLink href={link}>
         <a>
-          <ArticleTitle>{blog.title}</ArticleTitle>
+          <div className='py-4 px-8'>
+            <ArticleTitle>{blog.title}</ArticleTitle>
 
-          <Paragraph className='!my-0 opacity-60 font-primary text-base'>
-            {date}
-          </Paragraph>
+            <Paragraph className='!my-0 opacity-60 font-primary text-base'>
+              {date}
+            </Paragraph>
+          </div>
         </a>
       </NextLink>
     </li>

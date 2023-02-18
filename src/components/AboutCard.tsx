835e63ddc3eeeb6252c97c6f83263a867bf80692
Kirill Kurko <kirillkurko2609@gmail.com>
2023-02-18 23:04:43 +0300

    Implement about page

---
+++ src/components/AboutCard.tsx
@@ -0,0 +1,38 @@
+import { Paragraph } from '@components/typography';
+import classnames from 'classnames';
+
+interface Props {
+  avatar: string;
+  username: string;
+  message: string[];
+}
+
+const AboutCard = ({ avatar, username, message }: Props) => {
+  return (
+    <li className='mb-4 border border-neutral-700 rounded-md'>
+      <div className='p-4 flex flex-row'>
+        <div
+          className={classnames(
+            'w-12 h-12 min-w-[3rem] mr-4 bg-gradient-to-r rounded-full',
+            avatar,
+          )}
+        />
+        <div>
+          <Paragraph className='!mb-2 font-primary text-base font-bold leading-none'>
+            {username}
+          </Paragraph>
+          {message.map((item, index) => (
+            <Paragraph
+              key={index}
+              className='text-neutral-400 font-primary text-base last:mb-0'
+            >
+              {item}
+            </Paragraph>
+          ))}
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default AboutCard;
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,5 +1,31 @@
 import Head from 'next/head';
-import WorkInProgress from '@components/WorkInProgress';
+import { PageTitle, Paragraph } from '@components/typography';
+import AboutCard from '@components/AboutCard';
+
+interface Feedback {
+  avatar: string;
+  username: string;
+  message: string[];
+}
+
+const data: Feedback[] = [
+  {
+    avatar: 'from-amber-300 to-amber-700',
+    username: 'knowing-pelican118',
+    message: [
+      `Well, I’ve heard that penguins are intelligent, but this individual seems to be on top of his kind. Kirill is one of the most inspiring personalities that I have ever met. I’d like to say that he is an outstanding developer - but I can hardly read the code. However, I can say for sure that he is a perfect problem solver - no matter what kind of problems we face, if Kirill joins the discussion I feel safe.`,
+      `I know for sure that every developer who knows Kirill says that he has extraordinary deep knowledge of the subject. He has an ability to absorb information at unprecedented rates - there must be some magic involved, I see no other explanation. But what I like most about Kirill is his inner child who is willing to play all day long but with one correction - this child plays with code 🙂`,
+    ],
+  },
+  {
+    avatar: 'from-rose-300 to-rose-500',
+    username: 'detailed-lion933',
+    message: [
+      `You know, there are people who you look at and understand that everything will be cool for them. As cool as they imagine it to be. People usually say about them, "dude, this is too much, stop" or "man, are you crazy??" For them, the world has no limits. And the funny thing is, you can't even imagine how they will do it. You just KNOW that they will reach their final destination. Sooner or later. It's some kind of magic.`,
+      `I only know three people like that. Kirill is the first on the list.`,
+    ],
+  },
+];
 
 const About = () => {
   return (
@@ -8,8 +34,26 @@ const About = () => {
         <title>About | Kirill Kurko</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
-      <WorkInProgress />
+      <section className='mt-8 mb-12 inline-block'>
+        <header className='mb-8'>
+          <PageTitle>About</PageTitle>
+          <Paragraph className='text-neutral-400'>
+            This is the board on which the battle for the title of the best
+            emoji takes place. Click on an item with your favorite emoji to
+            support it.
+          </Paragraph>
+        </header>
+        <ol className='list-none m-0 p-0'>
+          {data.map(({ avatar, username, message }) => (
+            <AboutCard
+              key={username}
+              avatar={avatar}
+              username={username}
+              message={message}
+            />
+          ))}
+        </ol>
+      </section>
     </div>
   );
 };

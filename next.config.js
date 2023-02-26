b6b52380f51b72af6a091ee79ba12d047f23894f
Kirill Kurko <kirillkurko2609@gmail.com>
2023-02-26 08:59:18 +0300

    Redesign guestbook card

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatar.vercel.sh',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 module.exports = withContentlayer(nextConfig);

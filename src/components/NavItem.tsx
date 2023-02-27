027360ab07c2656647c1ff13d1800014fdfc543c
Kirill Kurko <kirillkurko2609@gmail.com>
2023-02-27 06:48:12 +0300

    Implement NavBar animation

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,26 +1,21 @@
-import { useRouter } from 'next/router';
 import NextLink from 'next/link';
 import clsx from 'clsx';
 
 interface Props {
   href: string;
   text: string;
+  isActive: boolean;
   disabled?: boolean;
 }
 
-const NavItem = ({ href, text, disabled = false }: Props) => {
-  const router = useRouter();
-  const isActive =
-    router.asPath === href ||
-    (href === '/blog' && router.pathname.includes('/blog'));
-
+const NavItem = ({ href, text, isActive, disabled = false }: Props) => {
   return (
     <NextLink
       href={href}
       aria-disabled={disabled}
       className={clsx(
-        'inline-block px-3 sm:py-2 rounded-lg font-medium transition-all text-neutral-400 hover:text-neutral-200',
-        isActive && 'bg-zinc-800 !text-neutral-200',
+        'inline-block px-3 sm:py-2 font-medium transition-all text-neutral-400 hover:text-neutral-200',
+        isActive && '!text-neutral-200',
       )}
     >
       <p className='capsize'>{text}</p>

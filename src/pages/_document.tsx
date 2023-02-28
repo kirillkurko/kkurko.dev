44d669d3b5d2c314ad8c7ac6dccc3ba3f72e4756
Kirill Kurko <kirillkurko2609@gmail.com>
2023-03-01 01:20:54 +0300

    Migrate to next/font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,16 +4,7 @@ export default class CustomDocument extends Document {
   render() {
     return (
       <Html lang='en' className='scroll-smooth bg-zinc-900'>
-        <Head>
-          <link
-            href='https://fonts.googleapis.com/css2?family=Red+Hat+Text:wght@400;500;700&display=swap'
-            rel='stylesheet'
-          />
-          <link
-            href='https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap'
-            rel='stylesheet'
-          />
-        </Head>
+        <Head />
         <body className='font-primary box-border max-[1200px]:overflow-auto'>
           <Main />
           <NextScript />
